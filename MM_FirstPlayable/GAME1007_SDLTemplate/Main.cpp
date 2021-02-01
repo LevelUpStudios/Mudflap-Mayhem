0206917ae5e04cd5510e47002587fe33df31806d
Miekole <Miekole.ferrazzo@gmail.com>
2021-02-01 15:51:57 -0500

    First Playable
    added background and sprites

---
+++ MM_FirstPlayable/GAME1007_SDLTemplate/Main.cpp
@@ -0,0 +1,13 @@
+
+// SDL includes pasted for convenience. Move/copy to relevant files.
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_ttf.h"
+#include "Engine.h"
+
+int main(int argc, char* argv[])
+{
+	Engine game;
+	return game.Run();
+}
